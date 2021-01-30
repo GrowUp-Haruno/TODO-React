cbaac1738c107e8fd2c8c621afdf9531fc041e04
GrowUp-Haruno <74647388+GrowUp-Haruno@users.noreply.github.com>
2021-01-30 22:29:01 +0900

    Initial commit

---
+++ src/components/CompleteTodo.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#ffffe0",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
+export const CompleteTodo = (props) => {
+  const { completeTodos, onClickBack } = props;
+
+  return (
+    <div style={style}>
+      <p className="title">完了のTODO</p>
+      <ul>
+        {completeTodos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button
+                onClick={() => {
+                  onClickBack(todo, index);
+                }}
+              >
+                戻す
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
